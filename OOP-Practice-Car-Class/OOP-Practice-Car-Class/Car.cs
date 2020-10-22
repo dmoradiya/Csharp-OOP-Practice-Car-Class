26397a2c71fc52fc8798a051bbf6240e2cb54c33
dhmec <dharmoradiya@gmail.com>
2020-10-22 12:23:02 -0600

    Created two new class : Odometer and FuelTank

---
+++ OOP-Practice-Car-Class/OOP-Practice-Car-Class/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Practice_Car_Class
+{
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int MaximumOccupancy { get; set; }
+        public double FuelEfficiency { get; set; }
+       
+
+    }
+}
